bb26522e6b0aa5ccfcc23fdd02bfcab37d8e4667
Ying Hu <36462094+yinghu-math@users.noreply.github.com>
2022-06-22 21:59:18 -0500

    initial commit

---
+++ app.py
@@ -0,0 +1,104 @@
+import plotly.graph_objects as go
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
+## dash version 1.19.0 (Dash 2.0 has different import statements)
+
+#from random import seed
+from random import randint
+import sorting_steps
+
+#seed(1)
+
+n = 50
+array = [randint(0, 50*n) for _ in range(n)]
+base_list = [i+1 for i in range(n)]
+
+method_lists = {'Merge Sort': sorting_steps.merge_sort_steps,
+                'Bubble Sort': sorting_steps.bubble_sort_steps}
+
+external_stylesheets = [
+    {
+        "href": "https://fonts.googleapis.com/css2?family=Comfortaa:wght@500&family=Lato&display=swap",
+        "rel": "stylesheet",
+    }
+]
+
+app = dash.Dash(__name__)
+app.title = "Sorting Algorithm Visualization."
+
+app.layout = html.Div(children = [
+    html.Div(
+        children = [
+                    html.H1(children="Sorting Algorithm Visualization",
+                    className="header-title")
+                    ],
+        className = "header",
+    ), ## header
+    html.Div(
+        children = [
+            html.Div(
+                children= [html.P("Stop the current animation first. \
+                Then select a different sorting algorithm: "),],
+                className="menu-title"),
+            dcc.Dropdown(
+                id="sorting_method",
+                options=[
+                    {"label": "Merge Sort", "value": "Merge Sort"},
+                    {"label": "Bubble Sort", "value": "Bubble Sort"},
+                ],
+                placeholder = "Select a sorting algorithm",
+                value = "Merge Sort",
+                className="dropdown",
+            ),
+        ],
+        className = "menu"
+    ), ## select menu
+    html.Div(
+        children = [
+            html.Div(dcc.Graph(id="sorting_illustration"), className="card")
+        ],
+        className="wrapper"
+    ), ## Graph dash_html_components
+])
+
+@app.callback(
+    Output(component_id="sorting_illustration", component_property="figure"),
+    Input(component_id="sorting_method", component_property="value")
+)
+def update_graph_title(sort_method):
+    steps = method_lists[sort_method](array)
+    fig = go.Figure(
+        data=[go.Bar(x=base_list, y = array[:], width=0.7),],
+        layout=go.Layout(
+            updatemenus=[
+                dict(type="buttons",
+                    buttons=[dict(label="Play",
+                                 method="animate",
+                                 args=[None,
+                                       {"frame": {"duration": 1},
+                                        "transition": {"duration": 0.5},
+                                        "mode": "immediate",
+                                       },
+                                      ],
+                                 ),
+                             dict(label="Stop",
+                                         method="animate",
+                                         args=['null',
+                                               {
+                                                "mode": "immediate",
+                                               },
+                                              ],
+                                         ),
+                            ],
+                ),
+            ],
+            height=500),
+        frames=[go.Frame(data=go.Bar(x=base_list, y=step)) for step in steps],
+        layout_yaxis_range=[0, 2600],
+    )
+    return fig
+
+if __name__ == "__main__":
+    app.run_server(debug=True)
---
+++ sorting_steps.py
@@ -0,0 +1,47 @@
+def bubble_sort_steps(array_original):
+    array = array_original[:]
+    n = len(array)
+    steps = [array[:], ]
+    flag = 1
+    while flag == 1:
+        flag = 0
+        for i in range(n-1):
+            if array[i] > array[i+1]:
+                array[i], array[i+1] = array[i+1], array[i]
+                flag = 1
+                steps.append(array[:])
+    return steps
+
+
+def merge_sort_steps(array_original, steps = None):
+## there are duplicate steps
+    array = array_original[:]
+    if steps is None:
+        steps = [array[:], ]
+    def merge_sort_helper(array, i, j, steps):
+        if j - i < 2:
+            return array[i : j]
+        c = (i + j) // 2
+        L = merge_sort_helper(array, i, c, steps)
+        R = merge_sort_helper(array, c, j, steps)
+        l = 0
+        r = 0
+        while l < (c - i) or r < (j - c):
+            if l < (c - i) and r < (j - c):
+                if L[l] <= R[r]:
+                    array[i + l + r] = L[l]
+                    l += 1
+                else:
+                    array[i + l + r] = R[r]
+                    r += 1
+            elif l == c-i:
+                array[i + l + r] = R[r]
+                r += 1
+            else:
+                array[i + l + r] = L[l]
+                l += 1
+            if array[:] not in steps:
+                steps.append(array[:])
+        return array[i : j]
+    merge_sort_helper(array, 0, len(array), steps)
+    return steps
